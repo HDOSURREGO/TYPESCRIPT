124e6ed49adef25f5863102718cb8be16005454d
Henry Hoyos <hdosurrego@gmail.com>
2020-05-22 17:36:53 -0400

    Type Annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,13 +1,19 @@
-let apples: number = 5; //": number" is the type annotation
-
+// Type Annotations
+//": number" is the type annotation
+let apples: number = 5;
 let speed: string = "fast";
-
 let hasName: boolean = true;
-
 let nothingMuch: null = null;
-
 let nothing: undefined = undefined;
 
 //built in objects
-
 let now: Date = new Date();
+
+//array
+let colors: string[] = ["red", "white", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+//class
+class Car {}
+let car: Car = new Car();
