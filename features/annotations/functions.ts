8e9ff9dd1e97e47e18f2c79b9bec2cfef62a7015
Henry Hoyos <hdosurrego@gmail.com>
2020-05-23 15:00:05 -0400

    Typing functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -18,3 +18,27 @@ function multiply(a: number, b: number): number {
 const divide = function (a: number, b: number): number {
 	return a / b;
 };
+
+const todayWeather = {
+	date: new Date(),
+	weather: "sunny",
+};
+
+const logWeather = (forecast: { date: Date; weather: string }): void => {
+	console.log(forecast.date);
+	console.log(forecast.weather);
+};
+
+logWeather(todayWeather);
+
+//Destructuring the function
+// const logWeather = ({
+// 	date,
+// 	weather,
+// }: {
+// 	date: Date;
+// 	weather: string;
+// }): void => {
+// 	console.log(date);
+// 	console.log(weather);
+// };
