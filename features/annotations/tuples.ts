d1cd783f2a1eb9883c85519d5586b78035720acd
Henry Hoyos <hdosurrego@gmail.com>
2020-05-25 13:21:12 -0400

    Tuples

---
+++ features/annotations/tuples.ts
@@ -0,0 +1,8 @@
+const drink = {
+	color: "brown",
+	carbonated: true,
+	sugar: 40,
+};
+
+const pepsi = ["brown", true, 40]; //This is an array
+const cocacola: [string, boolean, number] = ["brown", true, 40]; //This typing converts the array in a Tuple
