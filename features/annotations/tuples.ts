1bbd3f24fe2be88d5de8c6bb54e9c4d48685ea86
Henry Hoyos <hdosurrego@gmail.com>
2020-05-25 13:27:30 -0400

    Tuples

--- features/annotations/tuples.ts
+++ features/annotations/tuples.ts
@@ -6,3 +6,10 @@ const drink = {
 
 const pepsi = ["brown", true, 40]; //This is an array
 const cocacola: [string, boolean, number] = ["brown", true, 40]; //This typing converts the array in a Tuple
+
+//We can create a type-alias
+
+type Drink = [string, boolean, number];
+
+const sprite: Drink = ["clear", true, 40];
+const tea: Drink = ["brown", false, 0];
