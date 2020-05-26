e0f7682738e4a11fadeee2a1f1f7bbd4a805393b
Henry Hoyos <hdosurrego@gmail.com>
2020-05-26 18:48:25 -0400

    Interfaces

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -2,18 +2,22 @@ interface Vehicle {
 	name: string;
 	year: number;
 	broken: boolean;
+	summary(): string;
 }
 
+//In order to this object to be used in the printVehicle function as argument
+//it has to meet exactly the structure of the interface type Vehicle
 const oldCivic = {
 	name: "civic",
 	year: 2000,
 	broken: true,
+	summary(): string {
+		return `Name: ${this.name}`;
+	},
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-	console.log(`Name: ${vehicle.name}`);
-	console.log(`Year: ${vehicle.year}`);
-	console.log(`Broken: ${vehicle.broken}`);
+	console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic); //The oldCivic meets all the requirements of the Vehicle type
