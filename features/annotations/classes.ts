e1fc27f0e1629b8b7a03eb522b546e37fa26d77f
Henry Hoyos <hdosurrego@gmail.com>
2020-05-27 11:34:32 -0400

    classes

--- features/annotations/classes.ts
+++ features/annotations/classes.ts
@@ -1,17 +1,23 @@
 class Vehicle {
-	drive(): void {
+	public drive(): void {
 		console.log("chuga chuga");
+	}
+	public honk(): void {
+		console.log("beep");
 	}
 }
 
 const vehicle = new Vehicle();
 vehicle.drive();
+vehicle.honk();
 
 class Car extends Vehicle {
-	drive(): void {
+	//you can overwrite a function using the same modifier as the parent class
+	public drive(): void {
 		console.log("vroooom");
 	}
 }
 
 const carro = new Car();
 carro.drive();
+carro.honk();
